48565bb0ba664c3e6cfa5f2777ebe5a00b4ffa3f
zero-in USA <35350198+ZeroInUSA@users.noreply.github.com>
2018-12-25 03:11:59 -0600

    Add files via upload

---
+++ hello_world.c
@@ -0,0 +1,22 @@
+/*
+ * hello_world.c
+ *
+ *  Created on: Dec 24, 2018
+ *      Author: Zero In USA Dev 2
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+	int i = 0;
+	while (1){
+		i++;
+		printf("Loop %d\n",i);
+    	usleep(2000000);
+	}
+	exit(EXIT_SUCCESS);
+}
+
